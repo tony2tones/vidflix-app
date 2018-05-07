5e8528aadb3611fa36422f180e85f971608ab62e
tony2tones <anthony.marque@gmail.com>
2018-05-07 15:54:52 +0200

    sorting out particular project

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,49 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    context: __dirname,
-    entry: './js/ClientApp.jsx',
-    devtool: 'source-map',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    devServer: {
-        publicPath: './node_modules/webpack-dev-server/bin/webpack-dev-server.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json']
-    },
-    stats: {
-        colors: true,
-        reasons: true,
-        chunks: false
-    },
-    module: {
-        rules: [
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "eslint-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.jsx?$/,
-                loader: 'babel-loader',
+  context: __dirname,
+  entry: './js/ClientApp.js',
+  devtool: 'eval',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    publicPath: '/public/'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
+        include: path.resolve(__dirname, 'js'),
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
             }
+          }
         ]
-    }
-};
+      }
+    ]
+  }
+}
