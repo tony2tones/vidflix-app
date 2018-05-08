3e30ab19e5173a97b7379c28b6eb01ba254636c0
tony2tones <anthony.marque@gmail.com>
2018-05-08 09:21:18 +0200

    added Search component

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,11 +3,13 @@ import { render } from 'react-dom'
 import { HashRouter, Route } from 'react-router-dom'
 
 import Landing from './Landing'
+import Search from './Search'
 
 const App = () => (
   <HashRouter>
     <div className='app'>
       <Route extact path='/' component={Landing} />
+      <Route extact path='/search' component={Search} />
     </div>
   </HashRouter>
 )
